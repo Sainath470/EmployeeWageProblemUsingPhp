cdf0924709984414d26c3e030eb915ca15cc25fe
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-25 03:22:07 +0530

    ADDED : Part time wage

--- EmployeeWageComputation.php
+++ EmployeeWageComputation.php
@@ -4,13 +4,14 @@
  */
 $WAGE_PER_HOUR = 20;
 $FULL_TIME = 8;
+$PART_TIME = 4;
 
 /**
  * function to provide random number as attendance
  * returns random number either 1 or 2.
  */
 function employeeAttendance(){
-        return mt_rand(1,2);
+        return mt_rand(1,3);
 }
 
 /**
@@ -21,7 +22,11 @@ function employeeWageCalculation(){
     if($check == 1){
         echo "Employee is Present ";
         $totalWage = $GLOBALS['FULL_TIME'] * $GLOBALS['WAGE_PER_HOUR'];
-        echo "And Wage is: $totalWage";
+        echo "And Full time Wage is: $totalWage";
+    }else if ($check == 2){
+        echo "Employee is Present ";
+        $totalWage = $GLOBALS['PART_TIME'] * $GLOBALS['WAGE_PER_HOUR'];
+        echo "And Part time Wage is: $totalWage";
     }else{
         echo "Employee is Absent";
     }
