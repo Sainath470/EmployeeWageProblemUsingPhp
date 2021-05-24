9943486567c1650d10261077be7ef5bcaac0d662
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-25 02:51:48 +0530

    ADDED : function to check whether Employee is Present or not

--- EmployeeWageComputation.php
+++ EmployeeWageComputation.php
@@ -1,3 +1,26 @@
 <?php 
-echo "Welcome to the Employee Wage Computation Problem";
+/**
+ * Declaring varaible
+ */
+$randomCheck = 0;
+
+/**
+ * creating a function to check whether Employee is Present or Absent
+ * Using mt_rand() function. 
+ * Can also use rand()
+ */
+function checkingEmployeeAttendance(){
+    $randomCheck = mt_rand(1,2);
+    if($randomCheck == 1){
+        echo "Employee is Present";
+    }
+    else{
+        echo "Employee is Absent";
+    }
+}
+
+/**
+ * Calling the checkingEmployeeAttendance() function.
+ */
+checkingEmployeeAttendance();
 ?>
