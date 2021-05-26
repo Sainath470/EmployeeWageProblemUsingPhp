76c9c44943f92c55e33e6d81d88dbc867b2d546a
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-26 11:54:54 +0530

    ADDED : constructor and finding wage for multiple companies

--- EmployeeWageComputation.php
+++ EmployeeWageComputation.php
@@ -4,17 +4,28 @@ class EmployeeWageComputation{
     /**
      * Declaring constant properties and initializing
      */
-    const WAGE_PER_HOUR = 20;
-    const FULL_TIME = 8;
-    const PART_TIME = 4;
-    const TOTAL_WORKING_DAYS = 20;
-    const TOTAL_WORKING_HOURS = 100;
+    const FULL_TIME_HOURS = 8;
+    const PART_TIME_HOURS = 4;
+
+    private $company;
+    private $wagePerHour;
+    private $totalWorkingDays;
+    private $totalWorkingHours;
+
+    //constructor
+    public function __construct($company, $wagePerHour, $totalWorkingDays, $totalWorkingHours){
+        //Initialising the object
+        $this->company = $company;
+        $this->wagePerHour = $wagePerHour;
+        $this->totalWorkingDays = $totalWorkingDays;
+        $this->totalWorkingHours = $totalWorkingHours;
+    }
 
     /**
      * function to provide random number as attendance
      * returns random number either 1 or 2.
     */
-    function employeeAttendance(){
+    public function employeeAttendance(){
             return mt_rand(1,3);
     }
 
@@ -22,35 +33,40 @@ class EmployeeWageComputation{
      * Method to calculate employee Wage using switch statement
      * returning total wage of employee , working days and working hours;
      */
-    function employeeWageCalculation(){
+    public function employeeWageCalculation(){
         $workingDays = 0;
         $employeeHours =0;
 
-        While($workingDays < (self::TOTAL_WORKING_DAYS) && $employeeHours < (self::TOTAL_WORKING_HOURS)){
+        While($workingDays < $this->totalWorkingDays && $employeeHours < $this->totalWorkingHours){
             $check = $this->employeeAttendance();//calling method inside a method using this key word
             switch($check){
                 case 1:
-                    $employeeHours += self::FULL_TIME;
+                    $employeeHours += self::FULL_TIME_HOURS;
                     break;
                 case 2:
-                    $employeeHours += self::PART_TIME;
+                    $employeeHours += self::PART_TIME_HOURS;
                     break;
                 default:
                     break;
             }
             $workingDays++;
         }
-        $totalWage = $employeeHours * (self :: WAGE_PER_HOUR);
+        $totalWage = $employeeHours *  $this->wagePerHour;
 
-        echo "Total Employee Wage for 20 days is: $totalWage\n";
+        echo "Company name: " .$this->company. "\n";
+        echo "Total Employee Wage for $this->totalWorkingDays days is: $totalWage\n";
         echo "Total Employee Working Hours:" .$employeeHours."\n";
-        echo "Total Working days: " .$workingDays;
     }
 }
+
 /**
  * creating an object and calling the Method using the object
  */
-$employeeWageComputation = new EmployeeWageComputation();
+$employeeWageComputationForACompany = new EmployeeWageComputation("A-company", 20, 20, 100);
+$employeeWageComputationForBCompany = new EmployeeWageComputation("B-company", 15, 30, 120);
+$employeeWageComputationForCCompany = new EmployeeWageComputation("C-company", 25, 15, 80);
 
-$employeeWageComputation -> employeeWageCalculation();
+$employeeWageComputationForACompany -> employeeWageCalculation();
+$employeeWageComputationForBCompany -> employeeWageCalculation();
+$employeeWageComputationForCCompany -> employeeWageCalculation();
 ?>
