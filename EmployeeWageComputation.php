6e8eb5ec9a36eac2eecb38ec31c3064c80d2c256
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-26 11:08:33 +0530

    REFACTOR : implementing oops concept

--- EmployeeWageComputation.php
+++ EmployeeWageComputation.php
@@ -1,54 +1,56 @@
 <?php 
-/**
- * Declaring varaible
- */
-$WAGE_PER_HOUR = 20;
-$FULL_TIME = 8;
-$PART_TIME = 4;
-$TOTAL_WORKING_DAYS = 20;
-$TOTAL_WORKING_HOURS = 100;
 
-/**
- * function to provide random number as attendance
- * returns random number either 1 or 2.
- */
-function employeeAttendance(){
-        return mt_rand(1,3);
-}
+class EmployeeWageComputation{
+    /**
+     * Declaring constant properties and initializing
+     */
+    const WAGE_PER_HOUR = 20;
+    const FULL_TIME = 8;
+    const PART_TIME = 4;
+    const TOTAL_WORKING_DAYS = 20;
+    const TOTAL_WORKING_HOURS = 100;
 
-/**
- * function to calculate employee Wage using switch statement
- * returning total wage of employee , working days and working hours;
- */
-function employeeWageCalculation(){
-    $workingDays = 0;
-    $employeeHours =0;
-    global $TOTAL_WORKING_DAYS;
-    global $TOTAL_WORKING_HOURS;
-
-    While($workingDays < $TOTAL_WORKING_DAYS && $employeeHours < $TOTAL_WORKING_HOURS){
-        $check = employeeAttendance();
-        switch($check){
-            case 1:
-                $employeeHours += $GLOBALS['FULL_TIME'];
-                break;
-            case 2:
-                $employeeHours += $GLOBALS['PART_TIME'];
-                break;
-            default:
-                break;
-        }
-        $workingDays++;
+    /**
+     * function to provide random number as attendance
+     * returns random number either 1 or 2.
+    */
+    function employeeAttendance(){
+            return mt_rand(1,3);
     }
-    $totalWage = $employeeHours * $GLOBALS['WAGE_PER_HOUR'];
 
-    echo "Total Employee Wage for 20 days is: $totalWage\n";
-    echo "Total Employee Working Hours:" .$employeeHours."\n";
-    echo "Total Working days: " .$workingDays;
+    /**
+     * Method to calculate employee Wage using switch statement
+     * returning total wage of employee , working days and working hours;
+     */
+    function employeeWageCalculation(){
+        $workingDays = 0;
+        $employeeHours =0;
+
+        While($workingDays < (self::TOTAL_WORKING_DAYS) && $employeeHours < (self::TOTAL_WORKING_HOURS)){
+            $check = $this->employeeAttendance();//calling method inside a method using this key word
+            switch($check){
+                case 1:
+                    $employeeHours += self::FULL_TIME;
+                    break;
+                case 2:
+                    $employeeHours += self::PART_TIME;
+                    break;
+                default:
+                    break;
+            }
+            $workingDays++;
+        }
+        $totalWage = $employeeHours * (self :: WAGE_PER_HOUR);
+
+        echo "Total Employee Wage for 20 days is: $totalWage\n";
+        echo "Total Employee Working Hours:" .$employeeHours."\n";
+        echo "Total Working days: " .$workingDays;
+    }
 }
-
 /**
- * calling the function.
+ * creating an object and calling the Method using the object
  */
-employeeWageCalculation();
+$employeeWageComputation = new EmployeeWageComputation();
+
+$employeeWageComputation -> employeeWageCalculation();
 ?>
