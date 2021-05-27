cc9c5e85f0e86a6ef2223dd700b536e014ef5d59
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-27 11:49:19 +0530

    MODIFIED : employeeWage builder and create CompanyEmployee wage class

---
+++ CompanyEmployeeWage.php
@@ -0,0 +1,26 @@
+<?php
+class CompanyEmployeeWage{
+
+    const FULL_TIME_HOURS = 8;
+    const PART_TIME_HOURS = 4;
+     /**
+     * Declaring properties
+     */
+    public $companyName;
+    public $wagePerHour;
+    public $totalWorkingDays;
+    public $totalWorkingHours;
+
+
+    /**
+     * constructor to initialising the object
+     */
+    function __construct($companyName, $wagePerHour, $totalWorkingDays, $totalWorkingHours){
+        //Initialising the object
+        $this->companyName = $companyName;
+        $this->wagePerHour = $wagePerHour;
+        $this->totalWorkingDays = $totalWorkingDays;
+        $this->totalWorkingHours = $totalWorkingHours;
+    }
+}
+?>
